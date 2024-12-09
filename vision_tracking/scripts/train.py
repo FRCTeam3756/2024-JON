e63eb9e1c542d8f6744521e7d3c63107cd73dea6
Team Ramferno <3756ramferno@gmail.com>
2024-12-08 23:54:43 -0500

    Add files via upload

---
+++ vision_tracking/scripts/train.py
@@ -0,0 +1,28 @@
+import torch
+from ultralytics import YOLO
+
+def main():
+    best_device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("Running on device:", best_device)
+
+    model = YOLO('yolov11n/yolov11n.pt')
+
+    model.train(
+        data='yolov11n/scripts/dataset.yaml',
+        project='yolov11n/runs',
+        device=best_device,
+        epochs=50, 
+        imgsz=640, 
+        batch=16, 
+        workers=8,
+        exist_ok=True,
+        pretrained=True
+    )
+
+    results = model.val()
+    print("Validation Results:", results)
+
+    model.export(format='onnx')
+
+if __name__ == '__main__':
+    main()
